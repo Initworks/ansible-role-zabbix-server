3958fef282a55ff480642c9394df5162f883fea7
Cyvan Oort <cyvan@live.nl>
2020-08-26 21:23:46 +0000

    very secure password

--- roles/zabbix-server/files/runonce.sh
+++ roles/zabbix-server/files/runonce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 timescaledb-tune --quiet --yes
-sudo -u postgres createuser zabbix
+sudo -u postgres psql -c "create role zabbix with login password 'secret';"
 sudo -u postgres createdb -O zabbix zabbix
 zcat /usr/share/doc/zabbix-server-pgsql*/create.sql.gz | sudo -u zabbix psql zabbix
 echo "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;" | sudo -u postgres psql zabbix
